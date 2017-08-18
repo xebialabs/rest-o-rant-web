6ea0f7114ff1aa61a82105c224bf332f60a549aa
maneetxebia <mkaur@xebia.com>
2017-08-18 10:05:35 +0530

    DEPL-11351: Make rest-o-rant web look even nicer

--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -2,5 +2,34 @@ var restorantServices = angular.module('restorantServices', []);
 
 restorantServices.factory('Restaurant', ['$resource',
   function($resource){
-    return $resource('/api/findrestaurants');
+    return $resource('/api/findrestaurants', {}, {
+            query: {
+                method: 'GET',
+                responseType: 'json',
+                isArray: true,
+                params: {},
+                transformResponse: function (response) {
+                    var locations = ["Grachtengordel", "Binnenstad/Burgwallen"];
+                    var cuisines = ["European", "Steak"];
+                    var images = ["img/rest-o-rant-image1.jpg", "img/rest-o-rant-image2.jpg"];
+
+                    function randomNumber(maximumNumber){
+                      return Math.floor(Math.random() * (maximumNumber));
+                    }
+
+                    var wrapped = angular.fromJson(response);
+                     angular.forEach(wrapped, function(item) {
+                       item.image = images[randomNumber(images.length)];
+                       item.rating = randomNumber(5) + 1;
+                       item.reviews = randomNumber(200);
+                       item.recommended = randomNumber(100) + 1;
+                       item.cuisine = cuisines[randomNumber(cuisines.length)];
+                       item.location = locations[randomNumber(locations.length)];
+                       item.booked = randomNumber(20);
+                     });
+                     return wrapped;
+
+                }
+            }
+        });
 }]);
